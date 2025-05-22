4b2fe591a99f26d514b9066cd880300127c9b1ee
DT Nesimi <ditinesimi@gmail.com>
2025-05-21 21:39:42 -0400

    fixed coordinates not showing

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -23,8 +23,8 @@ int main()
 	bool draw = false;
 	bool byebye = false;
 
-	int xCord = width;
-	int yCord = height;
+	int xCord = width / 2;
+	int yCord = height / 2;
 
 	ALLEGRO_EVENT_QUEUE* eventQueue = NULL;
 
@@ -49,11 +49,32 @@ int main()
 
 	//game loop!!
 	while (!byebye) {
+
+		//creates a lil UPA to move
+		al_draw_filled_circle(xCord, yCord, 40, al_map_rgb(150, 150, 150)); //body
+		al_draw_filled_circle(xCord, yCord - 5, 2, al_map_rgb(0, 0, 0)); //nose
+		al_draw_filled_circle(xCord + 25, yCord - 5, 2, al_map_rgb(0, 0, 0)); //right eye
+		al_draw_filled_circle(xCord - 25, yCord - 5, 2, al_map_rgb(0, 0, 0)); //left eye
+		al_draw_line(xCord - 40, yCord + 15, xCord + 40, yCord + 15, al_map_rgb(0, 0, 0), 3); //mouth
+		al_draw_line(xCord, yCord + 2, xCord - 10, yCord + 10, al_map_rgb(0, 0, 0), 2);
+		al_draw_line(xCord - 10, yCord + 10, xCord - 20, yCord + 2, al_map_rgb(0, 0, 0), 2);
+		al_draw_line(xCord, yCord + 2, xCord + 10, yCord + 10, al_map_rgb(0, 0, 0), 2);
+		al_draw_line(xCord + 10, yCord + 10, xCord + 20, yCord + 2, al_map_rgb(0, 0, 0), 2);
+		al_draw_filled_circle(xCord + 30, yCord - 30, 15, al_map_rgb(150, 150, 150)); //right ear
+		al_draw_filled_circle(xCord - 30, yCord - 30, 15, al_map_rgb(150, 150, 150)); //left ear
+		al_draw_circle(xCord + 30, yCord - 30, 15, al_map_rgb(0, 0, 0), 5); //right outline
+		al_draw_circle(xCord - 30, yCord - 30, 15, al_map_rgb(0, 0, 0), 5); //left outline
+
+		al_flip_display();
+
 		ALLEGRO_EVENT event;
 		al_wait_for_event(eventQueue, &event);
+
+		//check if closing
 		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			byebye = true;
 		}
+		//store coords upon click for later, then draw smth
 		else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
 			if (event.mouse.button & 1) {
 				xCord = event.mouse.x;
@@ -63,15 +84,35 @@ int main()
 		}
 
 		if (draw) {
-			al_draw_textf(font24, al_map_rgb(255, 255, 255), xCord, yCord, ALLEGRO_ALIGN_LEFT, "Mouse at: %i, %i", xCord, yCord);
-			al_flip_display();
+			
+			if (xCord <= 200 && xCord >= 0 && yCord >= 0 && yCord <= 200) {
+				al_draw_filled_rectangle(0, 0, 200, 200, al_map_rgb(150, 150, 150));
+				al_draw_filled_rectangle(400, 400, 640, 480, al_map_rgb(0, 0, 0));
+				al_draw_textf(font24, al_map_rgb(255, 255, 255), xCord, yCord,
+					ALLEGRO_ALIGN_LEFT, "The mouse is located at = %i / %i", xCord, yCord);
+				al_flip_display();
+			}
+			else if (xCord >= 400 && xCord <= 640 && yCord >= 400 && yCord <= 480) {
+				al_draw_filled_rectangle(400, 400, 640, 480, al_map_rgb(150, 0, 150));
+				al_draw_filled_rectangle(0, 0, 200, 200, al_map_rgb(0, 0, 0));
+				al_draw_textf(font24, al_map_rgb(255, 255, 255), xCord, yCord,
+					ALLEGRO_ALIGN_LEFT, "The mouse is located at = %i / %i", xCord, yCord);
+				al_flip_display();
+			}
+			else {
+				al_draw_filled_rectangle(0, 0, 200, 200, al_map_rgb(0, 0, 0));
+				al_draw_filled_rectangle(400, 400, 640, 480, al_map_rgb(0, 0, 0));
+				al_draw_textf(font24, al_map_rgb(255, 255, 255), xCord, yCord,
+					ALLEGRO_ALIGN_LEFT, "The mouse is located at = %i / %i", xCord, yCord);
+				al_flip_display();
+			}
 			draw = false;
 		}
 
 		al_clear_to_color(al_map_rgb(0, 0, 0));
 	}
 
-
+	al_destroy_event_queue(eventQueue);
 	al_destroy_display(Screen);
 	return 0;
 }
